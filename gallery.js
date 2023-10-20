159c56ff304dcd6a42ac4df1bad474e0c149224d
lanroo <yladacs@gmail.com>
2023-10-20 20:06:33 -0300

    search auto ok

--- gallery.js
+++ gallery.js
@@ -6,14 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const enlargedPhoto = modal.querySelector('img');
     const closeButton = modal.querySelector('button');
     const gallery = document.querySelector('.gallery');
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+    const searchForm = document.getElementById('search-form'); // Adicione esta linha
 
     // Função para fechar o modal
     function closeModal() {
         modal.style.display = 'none';
     }
-    
+
     // Adicione o event listener para o botão Close
-    document.querySelector('.modal button').addEventListener('click', closeModal);
+    closeButton.addEventListener('click', closeModal);
 
     // Função para adicionar novas imagens à galeria
     function addImages() {
@@ -38,114 +41,30 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const photos = document.querySelectorAll('.photo');
-        const modal = document.querySelector('.modal');
-        const enlargedPhoto = modal.querySelector('img');
-        const closeButton = modal.querySelector('button');
-        const gallery = document.querySelector('.gallery');
-    
-        // Função para fechar o modal
-        function closeModal() {
-            modal.style.display = 'none';
-        }
-    
-        // Adicione um evento de clique a cada foto
-        photos.forEach(photo => {
-            photo.addEventListener('click', function() {
-                const backgroundImage = this.style.backgroundImage;
-                const imageUrl = backgroundImage.slice(5, backgroundImage.length - 2);
-                
-                enlargedPhoto.src = imageUrl;
-                modal.style.display = 'flex';
-            });
-        });
-    
-        // Adicione um evento de clique ao botão "Close" para fechar a modal
-        closeButton.addEventListener('click', closeModal);
-    
-        // Função para adicionar novas imagens à galeria
-        function addImages() {
-            for (let i = 0; i < 10; i++) { // Adiciona 10 imagens de cada vez
+    // Função para pesquisar imagens com base no termo de pesquisa
+    function searchImages() {
+        const searchTerm = searchInput.value;
+
+        fetch(`https://api.unsplash.com/photos/random?client_id=${UNSPLASH_ACCESS_KEY}&count=10&query=${searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+            gallery.innerHTML = ''; // Limpa a galeria atual
+            data.forEach(photo => {
                 const photoDiv = document.createElement('div');
                 photoDiv.className = 'photo';
-                const randomNum = Math.floor(Math.random() * 1000); // Gera um número aleatório para obter imagens diferentes
-                photoDiv.style.backgroundImage = `url('https://source.unsplash.com/random/${randomNum}')`;
+                photoDiv.style.backgroundImage = `url('${photo.urls.small}')`;
                 gallery.appendChild(photoDiv);
-            }
-        }
-    
-        // Adiciona imagens iniciais
-        addImages();
-    
-        // Detecta quando o usuário está quase no final da página
-        window.addEventListener('scroll', function() {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) { // 500 é um valor de "buffer" que você pode ajustar
-                addImages();
-            }
+
+                // Adicione o evento de clique à nova imagem
+                photoDiv.addEventListener('click', function() {
+                    enlargedPhoto.src = photo.urls.full;
+                    modal.style.display = 'flex';
+                });
+            });
+        })
+        .catch(error => {
+            console.error("Houve um erro ao buscar as imagens:", error);
         });
-    });
-
-    // evento de clique a cada foto
-    photos.forEach(photo => {
-        photo.addEventListener('click', function() {
-            const backgroundImage = this.style.backgroundImage;
-            const imageUrl = backgroundImage.slice(5, backgroundImage.length - 2);
-            
-            enlargedPhoto.src = imageUrl;
-            modal.style.display = 'flex';
-        });
-    });
-
-    // evento de clique ao botão "Close" para fechar a modal
-    closeButton.addEventListener('click', closeModal);
-    
-
-    // Adiciona imagens iniciais
-    addImages();
-
-    // Detecta quando o usuário está quase no final da página
-    window.addEventListener('scroll', function() {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) { 
-            addImages();
-        }
-    });
-});
-document.addEventListener('DOMContentLoaded', function() {
-    const photos = document.querySelectorAll('.photo');
-    const modal = document.querySelector('.modal');
-    const enlargedPhoto = modal.querySelector('img');
-    const closeButton = modal.querySelector('button');
-    const gallery = document.querySelector('.gallery');
-
-    // Função para fechar o modal
-    function closeModal() {
-        modal.style.display = 'none';
-    }
-
-    // Adicione um evento de clique a cada foto
-    photos.forEach(photo => {
-        photo.addEventListener('click', function() {
-            const backgroundImage = this.style.backgroundImage;
-            const imageUrl = backgroundImage.slice(5, backgroundImage.length - 2);
-            
-            enlargedPhoto.src = imageUrl;
-            modal.style.display = 'flex';
-        });
-    });
-
-    // Adicione um evento de clique ao botão "Close" para fechar a modal
-    closeButton.addEventListener('click', closeModal);
-
-    // Função para adicionar novas imagens à galeria
-    function addImages() {
-        for (let i = 0; i < 10; i++) { // Adiciona 10 imagens de cada vez
-            const photoDiv = document.createElement('div');
-            photoDiv.className = 'photo';
-            const randomNum = Math.floor(Math.random() * 1000); // Gera um número aleatório para obter imagens diferentes
-            photoDiv.style.backgroundImage = `url('https://source.unsplash.com/random/${randomNum}')`;
-            gallery.appendChild(photoDiv);
-        }
     }
 
     // Adiciona imagens iniciais
@@ -153,8 +72,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Detecta quando o usuário está quase no final da página
     window.addEventListener('scroll', function() {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) { // 500 é um valor de "buffer" que você pode ajustar
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
             addImages();
         }
+    });
+
+    // Adicione um evento de clique ao botão de pesquisa
+    searchButton.addEventListener('click', searchImages);
+
+    // Adicione um evento de envio de formulário ao formulário de pesquisa
+    searchForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o envio do formulário
+        searchImages();
     });
 });
