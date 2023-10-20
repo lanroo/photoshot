8e7659260498d6fa9f9109c1dfcc56bb993c5674
lanroo <yladacs@gmail.com>
2023-10-20 17:46:15 -0300

    fix bugs and improvements

--- gallery.js
+++ gallery.js
@@ -1,3 +1,5 @@
+const UNSPLASH_ACCESS_KEY = 'qa7HUlX2aBAficyO4Q-fj1PvA2ZyW-r2GCbMzGQW6xI';
+
 document.addEventListener('DOMContentLoaded', function() {
     const photos = document.querySelectorAll('.photo');
     const modal = document.querySelector('.modal');
@@ -8,6 +10,32 @@ document.addEventListener('DOMContentLoaded', function() {
     // Função para fechar o modal
     function closeModal() {
         modal.style.display = 'none';
+    }
+    
+    // Adicione o event listener para o botão Close
+    document.querySelector('.modal button').addEventListener('click', closeModal);
+
+    // Função para adicionar novas imagens à galeria
+    function addImages() {
+        fetch(`https://api.unsplash.com/photos/random?client_id=${UNSPLASH_ACCESS_KEY}&count=10`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(photo => {
+                const photoDiv = document.createElement('div');
+                photoDiv.className = 'photo';
+                photoDiv.style.backgroundImage = `url('${photo.urls.small}')`;
+                gallery.appendChild(photoDiv);
+
+                // Adicione o evento de clique à nova imagem
+                photoDiv.addEventListener('click', function() {
+                    enlargedPhoto.src = photo.urls.full;
+                    modal.style.display = 'flex';
+                });
+            });
+        })
+        .catch(error => {
+            console.error("Houve um erro ao buscar as imagens:", error);
+        });
     }
 
     // evento de clique a cada foto
@@ -23,17 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // evento de clique ao botão "Close" para fechar a modal
     closeButton.addEventListener('click', closeModal);
-
-    // Função para adicionar novas imagens à galeria
-    function addImages() {
-        for (let i = 0; i < 10; i++) { // Adiciona 10 imagens de cada vez
-            const photoDiv = document.createElement('div');
-            photoDiv.className = 'photo';
-            const randomNum = Math.floor(Math.random() * 1000); // Gera um número aleatório para obter imagens diferentes
-            photoDiv.style.backgroundImage = `url('https://source.unsplash.com/random/${randomNum}')`;
-            gallery.appendChild(photoDiv);
-        }
-    }
+    
 
     // Adiciona imagens iniciais
     addImages();
